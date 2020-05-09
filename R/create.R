07ae0329fc399fcc04fdf2e38db74cb4e11a88c2
Matt Dray <mwdray@gmail.com>
2020-05-09 16:11:59 +0100

    Try an ext_override arg

--- R/create.R
+++ R/create.R
@@ -29,6 +29,8 @@
 #' @param logo_path URL string for your custom logo image. (Use either a named
 #'     logo with \code{logo_simple} or a custom logo with \code{logo_path}; not
 #'     both.)
+#' @param ext_override Provide a file extension type for your logo_path that
+#'     overrides the one in \code{logo_path}.
 #' @param browser_preview Do you want to preview the badge in the browser? Requires
 #'     an internet connection.
 #' @param include_md Do you want to prepare the URL with Markdown syntax
@@ -60,6 +62,7 @@ get_badge <- function(
   logo_color = NULL,
   logo_width = NULL,
   logo_path = NULL,
+  ext_override = NULL,
   browser_preview = TRUE,
   include_md = TRUE,
   to_clipboard = TRUE
@@ -144,12 +147,21 @@ get_badge <- function(
     logo_path_64 <- base64enc::base64encode(logo_path)
 
     # Add path to badge URL
-    badge_url <- paste0(
-      badge_url,
-      "&logo=data:image/",
-      tools::file_ext(gsub("/$", "", logo_path)),  # file extension
-      ";base64,", logo_path_64
-    )
+    if (is.null(ext_override)) {
+      badge_url <- paste0(
+        badge_url,
+        "&logo=data:image/",
+        tools::file_ext(gsub("/$", "", logo_path)),  # file extension
+        ";base64,", logo_path_64
+      )
+    } else if (!is.null(ext_override)) {
+      badge_url <- paste0(
+        badge_url,
+        "&logo=data:image/",
+        ext_override,  # file extension override
+        ";base64,", logo_path_64
+      )
+    }
 
   }
 
